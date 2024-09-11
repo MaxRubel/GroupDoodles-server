8fe84274583b35ee8817a1fc38a2f4d64323a3ab
Max Rubel <maxrubel1@gmail.com>
2024-09-11 17:27:11 -0500

    added csrf_trusted_origins

--- groupdoodles/settings.py
+++ groupdoodles/settings.py
@@ -24,6 +24,7 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
 # CORS_ORIGIN_WHITELIST = ['*']
 
 # Quick-start development settings - unsuitable for production
